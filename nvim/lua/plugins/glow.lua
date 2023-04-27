ae08fbfc35fc357fdf80d4a5fd0cdf350c9c6e7e
ptrptrd <pptrd@protonmail.com>
2023-04-27 23:06:55 +0200

    add markdown previewer

--- nvim/lua/plugins/glow.lua
+++ nvim/lua/plugins/glow.lua
@@ -2,6 +2,12 @@ return {
 	'ellisonleao/glow.nvim',
 	config = true,
 	cmd = 'Glow',
+	opts = {
+		width = 300,
+		height = 100,
+		--width_ratio = 0.9,
+		height_ratio = 0.9,
+	},
 	keys = {
 		{'<leader>mp', '<cmd>Glow<cr>', desc = '[M]arkdown [P]review'},
 	}
